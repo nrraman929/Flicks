9a6f456d52953ea61d7f9cfc060689421fd0fe9f
Nishant Raman <Nishant@Nishants-MacBook-Pro.local>
2016-01-14 08:45:06 -0600

    initial commit

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Nishant Raman on 1/14/16.
+//  Copyright © 2016 Nishant Raman. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
