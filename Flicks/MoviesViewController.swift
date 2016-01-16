8321206ed6eeeede72ce76cb89da37f40ab21f98
Nishant Raman <Nishant@Nishants-MacBook-Pro.local>
2016-01-16 10:18:51 -0600

    required user stories

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
+import AFNetworking
+import MBProgressHUD
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
     var movies: [NSDictionary]?
+    var hud : MBProgressHUD?
+    var refreshControl: UIRefreshControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshControl, atIndex: 0)
 
         // Do any additional setup after loading the view.
         tableView.dataSource = self;
         tableView.delegate = self;
+        
+        
+        hud = MBProgressHUD.init(view: self.view)
+        hud!.color = UIColor.whiteColor()
+        hud!.activityIndicatorColor = UIColor.orangeColor()
+        self.view.addSubview(hud!)
+        hud?.show(true)
         
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
@@ -36,8 +51,8 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                     if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
                         data, options:[]) as? NSDictionary {
                             NSLog("response: \(responseDictionary)")
-                            
-                            self.movies = responseDictionary["results"] as! [NSDictionary]
+                            self.hud!.hide(true)
+                            self.movies = (responseDictionary["results"] as! [NSDictionary])
                             self.tableView.reloadData()
                     }
                 }
@@ -65,12 +80,37 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
         
+        let baseURL = "http://image.tmdb.org/t/p/w500"
+        let posterPath = movie["poster_path"] as? String
+        
+        var imageURL = NSURL(string: "")
+        if posterPath != nil { imageURL = NSURL(string: baseURL
+            + posterPath!)! }
+        
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
+        if imageURL!.absoluteString != "" {cell.posterView.setImageWithURL(imageURL!)}
+        else { cell.posterView.image = UIImage(named: "questionmark-512")}
+        
         
         //cell.textLabel!.text = title //"row \(indexPath.row)"
         print("row \(indexPath.row)")
         return cell
+    }
+    
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
+    }
+    
+    func onRefresh() {
+        delay(2, closure: {
+            self.refreshControl.endRefreshing()
+        })
     }
     
 
