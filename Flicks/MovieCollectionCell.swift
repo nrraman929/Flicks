8e422f51737d70176f437e97750d6ba2fd9280d6
Nishant Raman <Nishant@wireless-10-145-226-10.public.utexas.edu>
2016-01-28 18:44:21 -0600

    collection view

---
+++ Flicks/MovieCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionCell.swift
+//  Flicks
+//
+//  Created by Nishant Raman on 1/27/16.
+//  Copyright © 2016 Nishant Raman. All rights reserved.
+//
+
+import Cocoa
+
+class MovieCollectionCell: UICollectionViewCell {
+
+}
